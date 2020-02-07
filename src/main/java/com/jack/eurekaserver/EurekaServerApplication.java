964a24a494f04a0eb058704fc270a026ce8a3cbe
Jack Cai <cwx8900@gmail.com>
2020-02-07 22:50:05 +0800

    Initial commit

---
+++ src/main/java/com/jack/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.jack.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Jack
+ *
+ * http://localhost:7001/
+ * http://localhost:7002/
+ * http://localhost:7003/
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
